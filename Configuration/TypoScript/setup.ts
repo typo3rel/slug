0ab29dd9fdc2cf750086d692c3861f3ee45a1732
koehlersimon <simonkohler@192.168.2.1>
2019-07-07 13:13:15 -0500

    Added configuration for custom tables!

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -12,4 +12,30 @@ module.tx_slug {
         layoutRootPaths.0 = EXT:slug/Resources/Private/Backend/Layouts/
         layoutRootPaths.1 = {$module.tx_slug.view.layoutRootPath}
     }
+    settings{
+        additionalTables{
+            
+            tx_news_domain_model_news{
+                label = News
+                slugField = path_segment
+                titleField = title
+                icon = EXT:news/Resources/Public/Icons/news_domain_model_news.svg
+            }
+
+            tx_eventlist_domain_model_event{
+                label = Events
+                slugField = slug
+                titleField = title
+                icon = EXT:insta/Resources/Public/Icons/tx_eventlist_domain_model_event.gif
+            }
+
+            tx_gofaq_domain_model_faq{
+                label = FAQ
+                slugField = slug
+                titleField = title
+                icon = EXT:go_faq/Resources/Public/Icons/tx_gofaq_domain_model_faq.gif
+            }
+        
+        }
+    }
 }
