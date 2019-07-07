aa7ad2a050eb266a5faf1be86a396e5c95fb0aaa
koehlersimon <simonkohler@192.168.2.1>
2019-07-07 13:15:26 -0500

    Added "additionalTableAction" for any kind of given records

--- Classes/Controller/ExtensionController.php
+++ Classes/Controller/ExtensionController.php
@@ -22,14 +22,97 @@ class ExtensionController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionContro
     * @var HelperUtility
     */
     public $helper;
+    protected $backendConfiguration;
    
     
     /**
-    * @param PageRepository $pageRepository
+    * @param ExtensionRepository $extensionRepository
     */
     public function __construct(ExtensionRepository $extensionRepository) {
          $this->extensionRepository = $extensionRepository;
          $this->helper = GeneralUtility::makeInstance(HelperUtility::class);
+         $this->backendConfiguration = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('slug');
+    }
+    
+    public function additionalTableAction() {
+        
+        $backendConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('slug');
+        
+         // Check if filter variables are available, otherwise set default values from ExtensionConfiguration
+        if($this->request->hasArgument('filter')){
+            $filterVariables = $this->request->getArgument('filter');
+        }
+        else{ 
+            $filterVariables['maxentries'] = $backendConfiguration['recordMaxEntries'];
+            $filterVariables['orderby'] = $backendConfiguration['recordOrderBy'];
+            $filterVariables['order'] = $backendConfiguration['recordOrder'];
+            $filterVariables['key'] = '';
+        }
+        
+        // Set the order by options for fluid viewhelper f:form.switch
+        $filterOptions['orderby'] = [
+            ['value' => 'crdate', 'label' => $this->helper->getLangKey('filter.form.select.option.creation_date')],
+            ['value' => 'title', 'label' => $this->helper->getLangKey('filter.form.select.option.title')],
+            ['value' => 'path_segment', 'label' => $this->helper->getLangKey('filter.form.select.option.path_segment')],
+            ['value' => 'sys_language_uid', 'label' => $this->helper->getLangKey('filter.form.select.option.sys_language_uid')],
+        ];
+        
+        $filterOptions['order'] = [
+            ['value' => 'DESC', 'label' => $this->helper->getLangKey('filter.form.select.option.descending')],
+            ['value' => 'ASC', 'label' => $this->helper->getLangKey('filter.form.select.option.ascending')]
+        ];
+        
+        $filterOptions['maxentries'] = [
+            ['value' => '10', 'label' => '10'],
+            ['value' => '20', 'label' => '20'],
+            ['value' => '30', 'label' => '30'],
+            ['value' => '40', 'label' => '40'],
+            ['value' => '50', 'label' => '50'],
+            ['value' => '60', 'label' => '60'],
+            ['value' => '70', 'label' => '70'],
+            ['value' => '80', 'label' => '80'],
+            ['value' => '90', 'label' => '90'],
+            ['value' => '100', 'label' => '100'],
+            ['value' => '150', 'label' => '150'],
+            ['value' => '200', 'label' => '200'],
+            ['value' => '300', 'label' => '300'],
+            ['value' => '400', 'label' => '400'],
+            ['value' => '500', 'label' => '500']
+        ];
+        
+        if($this->request->hasArgument('table')){
+            $table = $this->request->getArgument('table');
+            if($this->extensionRepository->checkIfTableExists($table)){
+                $records = $this->extensionRepository->getAdditionalRecords(
+                        $table,
+                        $filterVariables,
+                        $this->settings['additionalTables']
+                        );
+
+                $this->view->assignMultiple([
+                    'filter' => $filterVariables,
+                    'filterOptions' => $filterOptions,
+                    'records' => $records,
+                    'table' => $this->request->getArgument('table')
+                ]);
+            }
+            else{
+                $this->view->assignMultiple([
+                    'message' => "Table doesn't exist!"
+                ]);
+            }
+        }
+        else{
+            $this->view->assignMultiple([
+                'message' => "Table argument not given!"
+            ]);
+        }
+        
+        $this->view->assignMultiple([
+            'backendConfiguration' => $backendConfiguration,
+            'additionalTables' => $this->settings['additionalTables']
+        ]);
+        
     }
     
     public function newsListAction() {
@@ -97,7 +180,9 @@ class ExtensionController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionContro
                 'filter' => $filterVariables,
                 'filterOptions' => $filterOptions,
                 'newsRecords' => $this->extensionRepository->getNewsList($filterVariables),
-                'extEmconf' => $this->helper->getEmConfiguration('slug')
+                'extEmconf' => $this->helper->getEmConfiguration('slug'),
+                'backendConfiguration' => $backendConfiguration,
+                'additionalTables' => $this->settings['additionalTables']
             ]);
         }
         
