80dda9b0a9704a183b0286bff6e2af0b6eb094e8
koehlersimon <simonkohler@192.168.2.1>
2019-07-08 13:57:22 -0500

    Important changes for filter options

--- Classes/Controller/ExtensionController.php
+++ Classes/Controller/ExtensionController.php
@@ -49,14 +49,6 @@ class ExtensionController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionContro
             $filterVariables['key'] = '';
         }
         
-        // Set the order by options for fluid viewhelper f:form.switch
-        $filterOptions['orderby'] = [
-            ['value' => 'crdate', 'label' => $this->helper->getLangKey('filter.form.select.option.creation_date')],
-            ['value' => 'title', 'label' => $this->helper->getLangKey('filter.form.select.option.title')],
-            ['value' => 'path_segment', 'label' => $this->helper->getLangKey('filter.form.select.option.path_segment')],
-            ['value' => 'sys_language_uid', 'label' => $this->helper->getLangKey('filter.form.select.option.sys_language_uid')],
-        ];
-        
         $filterOptions['order'] = [
             ['value' => 'DESC', 'label' => $this->helper->getLangKey('filter.form.select.option.descending')],
             ['value' => 'ASC', 'label' => $this->helper->getLangKey('filter.form.select.option.ascending')]
@@ -83,6 +75,15 @@ class ExtensionController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionContro
         if($this->request->hasArgument('table')){
             $table = $this->request->getArgument('table');
             if($this->extensionRepository->checkIfTableExists($table)){
+                
+                // Set the order by options for fluid viewhelper f:form.switch
+                $filterOptions['orderby'] = [
+                    ['value' => 'crdate', 'label' => $this->helper->getLangKey('filter.form.select.option.creation_date')],
+                    ['value' => $this->settings['additionalTables'][$table]['titleField'], 'label' => $this->helper->getLangKey('filter.form.select.option.title')],
+                    ['value' => $this->settings['additionalTables'][$table]['slugField'], 'label' => $this->helper->getLangKey('filter.form.select.option.path_segment')],
+                    ['value' => 'sys_language_uid', 'label' => $this->helper->getLangKey('filter.form.select.option.sys_language_uid')],
+                ];
+                
                 $records = $this->extensionRepository->getAdditionalRecords(
                         $table,
                         $filterVariables,
@@ -93,7 +94,10 @@ class ExtensionController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionContro
                     'filter' => $filterVariables,
                     'filterOptions' => $filterOptions,
                     'records' => $records,
-                    'table' => $this->request->getArgument('table')
+                    'table' => $table,
+                    'slugField' => $this->settings['additionalTables'][$table]['slugField'],
+                    'titleField' => $this->settings['additionalTables'][$table]['titleField'],
+                    'label' => $this->settings['additionalTables'][$table]['label']
                 ]);
             }
             else{
@@ -110,82 +114,10 @@ class ExtensionController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionContro
         
         $this->view->assignMultiple([
             'backendConfiguration' => $backendConfiguration,
-            'additionalTables' => $this->settings['additionalTables']
+            'additionalTables' => $this->settings['additionalTables'],
+            'extEmconf' => $this->helper->getEmConfiguration('slug'),
         ]);
         
     }
-    
-    public function newsListAction() {
-        
-        $tableExists = $this->extensionRepository->checkIfTableExists('tx_news_domain_model_news');
-        $backendConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('slug');
-        
-        // Check if filter variables are available, otherwise set default values from ExtensionConfiguration
-        if($this->request->hasArgument('filter')){
-            $filterVariables = $this->request->getArgument('filter');
-        }
-        else{ 
-            $filterVariables['maxentries'] = $backendConfiguration['newsMaxEntries'];
-            $filterVariables['orderby'] = $backendConfiguration['newsOrderBy'];
-            $filterVariables['order'] = $backendConfiguration['newsOrder'];
-            $filterVariables['key'] = '';
-        }
-        
-        // Set the order by options for fluid viewhelper f:form.switch
-        $filterOptions['orderby'] = [
-            ['value' => 'crdate', 'label' => $this->helper->getLangKey('filter.form.select.option.creation_date')],
-            ['value' => 'title', 'label' => $this->helper->getLangKey('filter.form.select.option.title')],
-            ['value' => 'path_segment', 'label' => $this->helper->getLangKey('filter.form.select.option.path_segment')],
-            ['value' => 'sys_language_uid', 'label' => $this->helper->getLangKey('filter.form.select.option.sys_language_uid')],
-        ];
-        
-        $filterOptions['order'] = [
-            ['value' => 'DESC', 'label' => $this->helper->getLangKey('filter.form.select.option.descending')],
-            ['value' => 'ASC', 'label' => $this->helper->getLangKey('filter.form.select.option.ascending')]
-        ];
-        
-        $filterOptions['maxentries'] = [
-            ['value' => '10', 'label' => '10'],
-            ['value' => '20', 'label' => '20'],
-            ['value' => '30', 'label' => '30'],
-            ['value' => '40', 'label' => '40'],
-            ['value' => '50', 'label' => '50'],
-            ['value' => '60', 'label' => '60'],
-            ['value' => '70', 'label' => '70'],
-            ['value' => '80', 'label' => '80'],
-            ['value' => '90', 'label' => '90'],
-            ['value' => '100', 'label' => '100'],
-            ['value' => '150', 'label' => '150'],
-            ['value' => '200', 'label' => '200'],
-            ['value' => '300', 'label' => '300'],
-            ['value' => '400', 'label' => '400'],
-            ['value' => '500', 'label' => '500']
-        ];
-        
-        // Checks first if the news table exists, then if the module is active
-        if(!$backendConfiguration['newsEnabled']){
-            $this->view->assignMultiple([
-                'extEmconf' => $this->helper->getEmConfiguration('slug'),
-                'message' => 'This module is not activated. Go to: Admin Tools -> Settings -> Extension Configuration and activate the module.'
-            ]);
-        }
-        elseif(!$tableExists){
-            $this->view->assignMultiple([
-                'extEmconf' => $this->helper->getEmConfiguration('slug'),
-                'message' => "Table 'tx_news_domain_model_news' doesn't exist",
-            ]);
-        }
-        else{
-            $this->view->assignMultiple([
-                'filter' => $filterVariables,
-                'filterOptions' => $filterOptions,
-                'newsRecords' => $this->extensionRepository->getNewsList($filterVariables),
-                'extEmconf' => $this->helper->getEmConfiguration('slug'),
-                'backendConfiguration' => $backendConfiguration,
-                'additionalTables' => $this->settings['additionalTables']
-            ]);
-        }
-        
-    }
-    
+ 
 }
