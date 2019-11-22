9cbc65f0a7f27ed1fc9a918e15c9aed4bc0d1d11
koehlersimon <simonkohler@192.168.2.1>
2019-11-22 16:47:20 -0500

    Changed version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-9.5.99',
+            'typo3' => '9.5.0-10.1.99',
         ],
         'conflicts' => [],
         'suggests' => [],
